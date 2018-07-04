325390c3070cdf2b338e09456bac8421ac326fbf
ricard <r.molina91@gmail.com>
2018-07-04 12:39:35 +0200

    Added simple formTemplate for creating clientes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
 -- clientes
 INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1, 'Ricard', 'Pepe', 'abc@gmail.com', '2018-03-10');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2, 'Pino', 'Juano', 'aasc@gmail.com', '2013-03-10');
