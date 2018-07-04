c90aaf67b6f519b8c4f047fdb14b68a58347c6aa
ricard <r.molina91@gmail.com>
2018-07-04 10:22:28 +0200

    Initial Commit Empty Project

---
+++ src/main/java/com/learning/facturas/app/FacturasAppApplication.java
@@ -0,0 +1,12 @@
+package com.learning.facturas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturasAppApplication.class, args);
+	}
+}
