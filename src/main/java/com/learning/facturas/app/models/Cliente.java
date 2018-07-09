f785b39d8e9c8c0320b024572c90a7c715e4a048
Ricard <r.molina91@gmail.com>
2018-07-09 13:20:25 +0200

    Added Factura Entity and mappings with Cliente

--- src/main/java/com/learning/facturas/app/models/Cliente.java
+++ src/main/java/com/learning/facturas/app/models/Cliente.java
@@ -1,14 +1,15 @@
 package com.learning.facturas.app.models;
 
 import org.springframework.format.annotation.DateTimeFormat;
-import org.thymeleaf.util.DateUtils;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Ricard on 04/07/2018.
@@ -32,11 +33,18 @@ public class Cliente implements Serializable {
 
     private String foto;
 
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "cliente")
+    private List<Factura> facturas;
+
     @Column(name = "create_at", nullable = false)
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern ="yyyy-MM-dd")
     @NotNull
     private Date createAt;
+
+    public Cliente() {
+        this.facturas = new ArrayList<Factura>();
+    }
 
     public Long getId() {
         return id;
@@ -84,6 +92,18 @@ public class Cliente implements Serializable {
 
     public void setFoto(String foto) {
         this.foto = foto;
+    }
+
+    public List<Factura> getFacturas() {
+        return facturas;
+    }
+
+    public void setFacturas(List<Factura> facturas) {
+        this.facturas = facturas;
+    }
+
+    public void addFactura(Factura factura) {
+        this.facturas.add(factura);
     }
 
     /*
---
+++ src/main/java/com/learning/facturas/app/models/Factura.java
@@ -0,0 +1,71 @@
+package com.learning.facturas.app.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Ricard on 09/07/2018.
+ */
+
+@Entity
+@Table(name = "facturas")
+public class Factura implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descripcion;
+    private String observacion;
+
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Cliente cliente;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.createAt = new Date();
+    }
+}
