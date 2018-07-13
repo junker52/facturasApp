9181665ebbd4d9244935ee4b9023546ccd3f3cf9
Ricard <r.molina91@gmail.com>
2018-07-13 16:22:17 +0200

    Handling with error on login

--- src/main/java/com/learning/facturas/app/controllers/LoginController.java
+++ src/main/java/com/learning/facturas/app/controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.learning.facturas.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
@@ -14,10 +15,13 @@ import java.security.Principal;
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(Model model, Principal principal, RedirectAttributes redirectAttributes) {
+    public String login(@RequestParam(value = "error", required = false) String error, Model model, Principal principal, RedirectAttributes redirectAttributes) {
         if (principal != null) {
             redirectAttributes.addFlashAttribute("success", "Sessión ya iniciada");
             return "redirect:/";
+        }
+        if (error != null) {
+            model.addAttribute("danger", "Login incorrecto");
         }
         return "login";
     }
