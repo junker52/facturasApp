89656824fe237bc3263a7e2ea0db6f3070b37380
Ricard <r.molina91@gmail.com>
2018-07-15 11:59:31 +0200

    Added BCryptPasswordEncoder

--- src/main/java/com/learning/facturas/app/FacturasAppApplication.java
+++ src/main/java/com/learning/facturas/app/FacturasAppApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class FacturasAppApplication implements CommandLineRunner {
@@ -12,6 +13,9 @@ public class FacturasAppApplication implements CommandLineRunner {
 
 	@Autowired
 	private UploadPictureService uploadPictureService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FacturasAppApplication.class, args);
@@ -21,5 +25,11 @@ public class FacturasAppApplication implements CommandLineRunner {
 	public void run(String... strings) throws Exception {
 		this.uploadPictureService.deleteAll();
 		this.uploadPictureService.init();
-	}
+
+        String pass = "123";
+        for (int i = 0; i < 2; i++) {
+            String encodedPass = this.bCryptPasswordEncoder.encode(pass);
+            System.out.println(encodedPass);
+        }
+    }
 }
--- src/main/java/com/learning/facturas/app/MvcConfig.java
+++ src/main/java/com/learning/facturas/app/MvcConfig.java
@@ -1,6 +1,8 @@
 package com.learning.facturas.app;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -29,5 +31,10 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/error_403").setViewName("error_403");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
